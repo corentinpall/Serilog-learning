a5ab8167f6253412f380f1b20a3dd4ffb8d854be
Corentin Pallard <corentin.pall@gmail.com>
2023-07-02 19:04:39 +0300

    Basic logger

---
+++ Logging.Core/LogDetail.cs
@@ -0,0 +1,30 @@
+﻿namespace Logging.Core;
+
+public class LogDetail
+{
+    public DateTime Timestamp { get; set; }
+    public string Message { get; set; }
+    
+    // WHERE
+    public string Product { get; set; }
+    public string Layer { get; set; }
+    public string Location { get; set; }
+    public string Hostname { get; set; }
+    
+    // WHO
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; }
+    
+    // EVERYTHING ELSE
+    public long? ElapsedMilliseconds { get; set; } // Only for performance entries
+    public Exception Exception { get; set; } // The exception for error logging
+    public string CorrelationId { get; set; } // Exception shielding from server to client
+    public Dictionary<string, object> AdditionalInfo { get; set; } // Catch-all for everything else
+
+    public LogDetail()
+    {
+        Timestamp = DateTime.Now;
+    }
+}
---
+++ Logging.Core/Logger.cs
@@ -0,0 +1,45 @@
+﻿using Serilog;
+using Serilog.Events;
+using System.Configuration;
+
+namespace Logging.Core;
+
+public static class Logger
+{
+    private static readonly ILogger _perfLogger;
+    private static readonly ILogger _usageLogger;
+    private static readonly ILogger _errorLogger;
+    private static readonly ILogger _diagnosticLogger;
+
+    static Logger()
+    {
+        _perfLogger = new LoggerConfiguration()
+            .WriteTo.File(path: "../logs/performance.txt").CreateLogger();
+        
+        _usageLogger = new LoggerConfiguration()
+            .WriteTo.File(path: "../logs/usage.txt").CreateLogger();
+        
+        _errorLogger = new LoggerConfiguration()
+            .WriteTo.File(path: "../logs/error.txt").CreateLogger();
+        
+        _diagnosticLogger = new LoggerConfiguration()
+            .WriteTo.File(path: "../logs/diagnostic.txt").CreateLogger();
+    }
+
+    public static void WritePerf(LogDetail infoToLog)
+        => _perfLogger.Write(LogEventLevel.Information, "@{LogDetail}", infoToLog);
+
+    public static void WriteUsage(LogDetail infoToLog)
+        => _usageLogger.Write(LogEventLevel.Information, "@{LogDetail}", infoToLog);
+
+    public static void WriteError(LogDetail infoToLog)
+        => _errorLogger.Write(LogEventLevel.Information, @"{LogDetail}", infoToLog);
+
+    public static void WriteDiagnostic(LogDetail infoToLog)
+    {
+        var writeDiagnostics = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableDiagnostics"]);
+        if (!writeDiagnostics) return;
+        
+        _diagnosticLogger.Write(LogEventLevel.Information, @"{LogDetail}", infoToLog);
+    }
+}
